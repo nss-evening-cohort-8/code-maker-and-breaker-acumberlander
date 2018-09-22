f52e3e7fd6b7c25d2520c4c166137a5a857f81eb
Austin Cumberlander <austincumberlander@gmail.com>
2018-09-22 13:48:34 -0500

    fixed typo on 'convert to message' button

--- main.js
+++ main.js
@@ -8,7 +8,28 @@ const printToDom = (stringToPrint, whereToPrint) => {
 const convertToSecret = document.getElementById("convertToSecret");
 const convertToMessage = document.getElementById("convertToMessage");
 
-//input fields -- NOT THEIR VALUE
-const secretToConvert = document.getElementById("secretToConvert");
-const messageToConvert = document.getElementById("messageToConvert");
+//input fields
+// const messageToConvert = document.getElementById("messageToConvert").value;
+const secretToConvert = document.getElementById("secretToConvert").value;
 
+
+// function that converts message to secret
+const messageToSecretConverter = () => {
+    let messageToConvert = document.getElementById("messageToConvert").value;
+    let messageArray = messageToConvert.split("");
+    // let index = 4;
+    for (i = 0; i < messageToConvert.length; i++) {
+        messageArray.push([i]);
+        // for (j = 0; j < messageArray.length; j++) {
+        //     i.charCodeAt([j]);
+        // }
+    }
+
+    printToDom(messageArray, 'convertedMessage');
+}
+
+convertToSecret.addEventListener('click', (e) => {
+    e.preventDefault();
+    messageToSecretConverter();
+});
+// let messageArray = messageToConvert.split("");
